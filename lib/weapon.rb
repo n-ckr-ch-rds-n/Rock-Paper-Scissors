066a031655997aef0583425e04272db3c0ca4768
Nick <nicholasjohnrichardson@gmail.com>
2018-06-04 21:24:12 +0100

    did some small things

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,7 +1,8 @@
 class Weapon
   def pick
-    return "Rock" if rand(3) + 1 == 1
-    return "Paper" if rand(3) + 1 == 2
-    return "Scissors" if rand(3) + 1 == 3
+    # return "Rock" if rand(3) + 1 == 1
+    # return "Paper" if rand(3) + 1 == 2
+    # return "Scissors" if rand(3) + 1 == 3
+    ["Rock", "Paper", "Scissors"].sample
   end
 end
